8f2679c052f1929bad0e9be0129564022575068e
Sam Mulube <sam@umbrellium.co.uk>
2015-05-08 14:37:15 +0100

    Copy paste the example page from the wercker blog post.

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	r "github.com/christopherhesse/rethinkgo"
+	"log"
+	"net/http"
+	"os"
+)
+
+var sessionArray []*r.Session
+
+type Bookmark struct {
+	Title string
+	Url   string
+}
+
+func initDb() {
+	session, err := r.Connect(os.Getenv("WERCKER_RETHINKDB_URL"), "gettingstarted")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = r.DbCreate("gettingstarted").Run(session).Exec()
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = r.TableCreate("bookmarks").Run(session).Exec()
+	if err != nil {
+		log.Println(err)
+	}
+
+	sessionArray = append(sessionArray, session)
+}
+
+func main() {
+	initDb()
+
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/new", insertBookmark)
+
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		log.Fatal("Error: %v", err)
+		return
+	}
+}
+
+func insertBookmark(res http.ResponseWriter, req *http.Request) {
+	session := sessionArray[0]
+
+	b := new(Bookmark)
+	json.NewDecoder(req.Body).Decode(b)
+
+	var response r.WriteResponse
+
+	err := r.Table("bookmarks").Insert(b).Run(session).One(&response)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	data, _ := json.Marshal("{'bookmark':'saved'}")
+	res.Header().Set("Content-Type", "application/json; charset=utf-8")
+	res.Write(data)
+}
+
+func handleIndex(res http.ResponseWriter, req *http.Request) {
+	session := sessionArray[0]
+	var response []Bookmark
+
+	err := r.Table("bookmarks").Run(session).All(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, _ := json.Marshal(response)
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Write(data)
+}
---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func init() {
+	initDb()
+}
+
+func TestHandleIndexReturnsWithStatusOK(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+
+	handleIndex(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Response body did not contain expected %v:\n\tbody: %v", "200", response.Code)
+	}
+}
+
+func TestHandleIndexReturnsJSON(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+
+	handleIndex(response, request)
+
+	ct := response.HeaderMap["Content-Type"][0]
+	if !strings.EqualFold(ct, "application/json") {
+		t.Fatalf("Content-Type does not equal 'application/json'")
+	}
+}
